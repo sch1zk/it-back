e78e7fc3fc682c6a11169fd34ef3811ddef5f17c
sch1zk <shizoidyt@gmail.com>
2025-02-17 01:08:36 +0700

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://127.0.0.1:3000',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
